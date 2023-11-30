5e9addde652057dbd763e0ace4799d4f1321f537
Ali Shan <optimist.alishan@gmail.com>
2023-12-01 03:59:56 +0500

    1024==>>1200

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+      screens: {
+        'sm': '640px',
+  
+        'md': '768px',
+  
+        'lg': '1200px',
+  
+        'xl': '1280px',
+  
+        '2xl': '1536px',
+      }
   },
   plugins: [],
 };
