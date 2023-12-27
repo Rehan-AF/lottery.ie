153312c61c65a8ebf471e6fdb8aaf02a55c2fe1e
Shan-barcha <shanbarcha11@gmil.com>
2023-12-27 21:01:11 +0500

    Fix: remaining Ticket page

--- src/components/Profile/MyTickets.jsx
+++ src/components/Profile/MyTickets.jsx
@@ -35,7 +35,7 @@ const MyTickets = () => {
 
             <div className="border-b border-gray-300  h-14 sm:hidden lg:flex items-center w-full justify-center lg:h-24">
                 <h1 className="font-black text-lg md:text-xl text-[#32444e]">
-                    My Details
+                    My Tickets
                 </h1>
             </div>
 
